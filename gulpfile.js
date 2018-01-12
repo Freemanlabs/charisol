c2e226c5479dd804169be04e8ce60cdb1898be85
Ekpo David <ekpodave@gmail.com>
2018-01-12 22:25:11 +0100

    Base commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber-notifier')
+
+gulp.task('css', function(){
+  gulp.src('assets/css/app.scss')
+      .pipe(plumber())
+      .pipe(sass({
+        outputStyle : 'compressed'
+      }))
+      .pipe(gulp.dest('assets/css/'))
+})
+
+gulp.task('watch', function(){
+  gulp.watch('assets/css/**/*.scss', ['css'])
+})
+
+gulp.task('default', ['css', 'watch'])
