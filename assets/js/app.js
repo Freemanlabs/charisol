3bf90f95167634ce9bfdeea3d618d345a2ef63a9
Ekpo David <ekpodave@gmail.com>
2018-01-17 00:36:54 +0100

    All pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,15 +8,33 @@ $(function(){
     'side': 'right',
     'tolerance': 70
   });
-  // Toggle button
+  if (document.getElementById('profile')){
+    var profilePanel = new Slideout({
+      'panel': document.getElementById('panel'),
+      'menu': document.getElementById('profile'),
+      'padding': 400,
+      'side': 'left',
+      'tolerance': 70
+    });
+  }
+  
+  // Toggle buttons
   $('.hamburger').on('click', function() {
     $(this).fadeToggle();
     slideoutDesktop.toggle();
   });
-  // close button
+  $('.team .btn-clear-purple').on('click', function() {
+    $('.content-cover').fadeIn();
+    profilePanel.toggle();
+  });
+
+  // close buttons
   $('#menu-desktop .close').on('click', function() {
-    // $('.content-cover').fadeOut();
     $('.hamburger').fadeToggle();
     slideoutDesktop.close();
+  });
+  $('#profile .close').on('click', function() {
+    $('.content-cover').fadeOut();
+    profilePanel.close();
   });
 });
