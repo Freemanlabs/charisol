5ea14a7b270880e15f9666496dff5a2c3de2763a
Ekpo David <ekpodave@gmail.com>
2018-01-18 14:13:22 +0100

    Mediaqueries for all pages, email support, modals, and link updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,18 @@
 /*global Slideout*/
 
 $(function(){
+  var deviceWidth = $(window).width(),
+      sliderWidth = 400;
+  
+  if (deviceWidth <= 425){
+    sliderWidth = 300;
+    $('#menu-desktop, #profile').addClass('mobile');
+  }
+
   var slideoutDesktop = new Slideout({
     'panel': document.getElementById('panel'),
     'menu': document.getElementById('menu-desktop'),
-    'padding': 400,
+    'padding': sliderWidth,
     'side': 'right',
     'tolerance': 70
   });
@@ -12,7 +20,7 @@ $(function(){
     var profilePanel = new Slideout({
       'panel': document.getElementById('panel'),
       'menu': document.getElementById('profile'),
-      'padding': 400,
+      'padding': sliderWidth,
       'side': 'left',
       'tolerance': 70
     });
@@ -21,20 +29,29 @@ $(function(){
   // Toggle buttons
   $('.hamburger').on('click', function() {
     $(this).fadeToggle();
+    $('#menu-desktop').toggleClass('show');
     slideoutDesktop.toggle();
   });
   $('.team .btn-clear-purple').on('click', function() {
     $('.content-cover').fadeIn();
+    $('#profile').toggleClass('show');
     profilePanel.toggle();
   });
 
   // close buttons
   $('#menu-desktop .close').on('click', function() {
     $('.hamburger').fadeToggle();
+    $('#menu-desktop').toggleClass('show');
     slideoutDesktop.close();
   });
   $('#profile .close').on('click', function() {
     $('.content-cover').fadeOut();
+    $('#profile').toggleClass('show');
     profilePanel.close();
   });
+
+  //panel height
+  if ($(document).height() < $(window).height()){
+    $('#panel').css('height', '100vh');
+  }
 });
