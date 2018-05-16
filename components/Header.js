f8d3a38a30f6a9112d35dec9a197ee18c42f3e78
charisol-oalex <alex.onozor@charisol.io>
2018-05-16 16:09:32 +0100

    fixing seo

--- components/Header.js
+++ components/Header.js
@@ -20,5 +20,8 @@ export default (props) => (
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
             <meta name="description" content="Charisol" />
+            <link rel="apple-touch-icon" sizes="180x180" href="../static/icon/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="../static/icon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="../static/icon/favicon-16x16.png" />
   </Head>
 )
